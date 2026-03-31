dabb93f2cd4e9413f4a79ea60faa28bf073b70b0
Theo <thobejane.theophiles@gmail.com>
2026-03-31 12:24:11 +0200

    dashboard action buttons

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -130,10 +130,10 @@ async function loadProjectsTable() {
         <td>${project.stars}</td>
         <td>
           <div class="table-actions">
-            <button class="btn btn-ghost" onclick="editProject(${project.id})" title="Edit">
+            <button class="btn btn-ghost" onclick="editProject(${project.id})" title="Edit" style="color: #ffbf00;">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
             </button>
-            <button class="btn btn-ghost" onclick="deleteProject(${project.id})" title="Delete" style="color: #ef4444;">
+            <button class="btn btn-ghost" onclick="deleteProject(${project.id})" title="Delete" style="color: #ff0000;">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/></svg>
             </button>
           </div>
